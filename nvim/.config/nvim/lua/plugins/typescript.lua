ea4bf8e9c8231f5da32e91b04a872b57c5aab385
Alexander Sapountzis <alexs@mparticle.com>
2025-08-15 12:29:44 -0400

    Add commit, dbee and ts lsp

---
+++ nvim/.config/nvim/lua/plugins/typescript.lua
@@ -0,0 +1,38 @@
+-- TypeScript/JavaScript customizations
+-- (The main typescript extra is imported in lazy.lua)
+return {
+  -- Customize TypeScript LSP settings
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
